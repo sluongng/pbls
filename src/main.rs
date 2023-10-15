c868d424ac7b441fe425b425c9398cdba67fd972
Ryan Roden-Corrent <ryan@rcorre.net>
2023-10-15 16:25:10 -0400

    Respond to request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use serde::{Deserialize, Serialize};
 use serde_json as json;
 use std::io;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Request {
+    id: u32,
+    method: String,
+    params: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    id: u32,
+}
+
 fn read_len() -> io::Result<usize> {
     for line in io::stdin().lines() {
-        match line.unwrap().strip_prefix("Content-Length: ") {
+        match line?.strip_prefix("Content-Length: ") {
             Some(l) => match l.parse::<usize>() {
                 Ok(sz) => return Ok(sz),
                 Err(err) => panic!("Failed to parse: {}", err),
@@ -15,14 +28,24 @@ fn read_len() -> io::Result<usize> {
     return Ok(0);
 }
 
-fn read_msg() -> json::Result<json::Value> {
-    let val: json::Value = serde_json::from_reader(io::stdin())?;
-    return Ok(val);
+fn read_request() -> json::Result<Request> {
+    let req: Request = json::from_reader(io::stdin())?;
+    return Ok(req);
+}
+
+fn reply(resp: Response) -> json::Result<()> {
+    let str = json::to_string(&resp)?;
+    print!("Content-Length: {}\r\n\r\n", str.len());
+    print!("{}\r\n\r\n", str);
+    return Ok(());
 }
 
 fn main() {
     eprintln!("starting spelgud");
-    eprintln!("got len: {}", read_len().unwrap());
-    eprintln!("got msg: {}", read_msg().unwrap());
-    return;
+    loop {
+        eprintln!("got len: {}", read_len().unwrap());
+        let req = read_request().unwrap();
+        eprintln!("got msg: {:?}", req);
+        reply(Response { id: req.id }).unwrap();
+    }
 }
