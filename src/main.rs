ef3025c68d9d3411f1c8602387f4ce55f547b7ca
Ryan Roden-Corrent <ryan@rcorre.net>
2023-10-15 15:12:32 -0400

    Read length header.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use serde_json::Value;
 use std::io;
 
-fn main() {
-    // Parse the string of data into serde_json::Value.
-    let v: Value = match serde_json::from_reader(io::stdin()) {
-        Ok(j) => j,
-        Err(err) => {
-            panic!("Failed to deserialize: {:?}", err)
+fn read_len() -> io::Result<usize> {
+    for line in io::stdin().lines() {
+        match line.unwrap().strip_prefix("Content-Length: ") {
+            Some(l) => match l.parse::<usize>() {
+                Ok(sz) => return Ok(sz),
+                Err(err) => panic!("Failed to parse: {}", err),
+            },
+            None => {}
         }
-    };
+    }
 
-    // Access parts of the data by indexing with square brackets.
-    println!("{}", v);
+    return Ok(0);
+}
+
+fn main() {
+    eprintln!("starting spelgud");
+    eprintln!("got len: {}", read_len().unwrap());
+    return;
+    // // Parse the string of data into serde_json::Value.
+    // let v: Value = match serde_json::from_reader(io::stdin()) {
+    //     Ok(j) => j,
+    //     Err(err) => {
+    //         panic!("Failed to deserialize: {:?}", err)
+    //     }
+    // };
+
+    // eprintln!("{}", v);
 }
