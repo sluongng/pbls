885ca070250a49400f0a7a149c38a327a325be16
Ryan Roden-Corrent <ryan@rcorre.net>
2023-10-21 13:12:05 -0400

    Copy lsp_server example.
    https://github.com/rust-lang/rust-analyzer/blob/7c11399b6bc5cf7a0b12a506a4b155a4be0458ee/lib/lsp-server/examples/goto_def.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,82 @@
-use lsp_types;
-use serde::{Deserialize, Serialize};
-use serde_json as json;
 use std::error::Error;
-use std::io::{self, Read};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Request {
-    id: u32,
-    method: String,
-    params: json::Value,
+use lsp_types::OneOf;
+use lsp_types::{
+    request::GotoDefinition, GotoDefinitionResponse, InitializeParams, ServerCapabilities,
+};
+
+use lsp_server::{Connection, ExtractError, Message, Request, RequestId, Response};
+
+fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
+    // Note that  we must have our logging only write out to stderr.
+    eprintln!("starting generic LSP server");
+
+    // Create the transport. Includes the stdio (stdin and stdout) versions but this could
+    // also be implemented to use sockets or HTTP.
+    let (connection, io_threads) = Connection::stdio();
+
+    // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
+    let server_capabilities = serde_json::to_value(&ServerCapabilities {
+        definition_provider: Some(OneOf::Left(true)),
+        ..Default::default()
+    })
+    .unwrap();
+    let initialization_params = connection.initialize(server_capabilities)?;
+    main_loop(connection, initialization_params)?;
+    io_threads.join()?;
+
+    // Shut down gracefully.
+    eprintln!("shutting down server");
+    Ok(())
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Response {
-    id: u32,
-    result: json::Value,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ServerInfo {
-    name: String,
-    version: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct InitializeResult {
-    capabilities: ServerInfo,
-    server_info: ServerInfo,
-}
-
-fn read_len() -> io::Result<usize> {
-    let mut len = 0;
-
-    for line in io::stdin().lines() {
-        let l = line?;
-        if l.is_empty() {
-            break;
-        }
-        match l.strip_prefix("Content-Length: ") {
-            Some(l) => match l.parse::<usize>() {
-                Ok(sz) => len = sz,
-                Err(err) => panic!("Failed to parse: {}", err),
-            },
-            None => {}
+fn main_loop(
+    connection: Connection,
+    params: serde_json::Value,
+) -> Result<(), Box<dyn Error + Sync + Send>> {
+    let _params: InitializeParams = serde_json::from_value(params).unwrap();
+    eprintln!("starting example main loop");
+    for msg in &connection.receiver {
+        eprintln!("got msg: {msg:?}");
+        match msg {
+            Message::Request(req) => {
+                if connection.handle_shutdown(&req)? {
+                    return Ok(());
+                }
+                eprintln!("got request: {req:?}");
+                match cast::<GotoDefinition>(req) {
+                    Ok((id, params)) => {
+                        eprintln!("got gotoDefinition request #{id}: {params:?}");
+                        let result = Some(GotoDefinitionResponse::Array(Vec::new()));
+                        let result = serde_json::to_value(&result).unwrap();
+                        let resp = Response {
+                            id,
+                            result: Some(result),
+                            error: None,
+                        };
+                        connection.sender.send(Message::Response(resp))?;
+                        continue;
+                    }
+                    Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    Err(ExtractError::MethodMismatch(req)) => req,
+                };
+                // ...
+            }
+            Message::Response(resp) => {
+                eprintln!("got response: {resp:?}");
+            }
+            Message::Notification(not) => {
+                eprintln!("got notification: {not:?}");
+            }
         }
     }
-
-    return Ok(len);
+    Ok(())
 }
 
-fn read_request(len: usize) -> Result<Request, Box<dyn Error>> {
-    let mut buf = vec![0; len];
-    io::stdin().read_exact(&mut buf)?;
-    let req: Request = json::from_slice(&buf)?;
-    return Ok(req);
-}
-
-fn reply(resp: Response) -> json::Result<()> {
-    let str = json::to_string(&resp)?;
-    print!("Content-Length: {}\r\n\r\n", str.len());
-    print!("{}\r\n\r\n", str);
-    return Ok(());
-}
-
-fn main() {
-    let result = lsp_types::InitializeResult {
-        capabilities: lsp_types::ServerCapabilities {
-            ..lsp_types::ServerCapabilities::default()
-        },
-        server_info: Some(lsp_types::ServerInfo {
-            name: String::from("spelgud"),
-            version: Some(String::from("0.1")),
-        }),
-    };
-    eprintln!("starting spelgud {:?}", json::to_string(&result));
-    loop {
-        let len = read_len().unwrap();
-        eprintln!("got len: {}", len);
-        let req = read_request(len).unwrap();
-        eprintln!("got msg: {:?}", req);
-        reply(Response {
-            id: req.id,
-            result: json::to_value(&result).unwrap(),
-        })
-        .unwrap();
-    }
+fn cast<R>(req: Request) -> Result<(RequestId, R::Params), ExtractError<Request>>
+where
+    R: lsp_types::request::Request,
+    R::Params: serde::de::DeserializeOwned,
+{
+    req.extract(R::METHOD)
 }
