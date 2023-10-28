95be25958538e9dd488cab33f5404d984506fe0c
Ryan Roden-Corrent <ryan@rcorre.net>
2023-10-28 16:21:40 -0400

    Parse aspell lines.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::error::Error;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::{error::Error, process::Stdio};
 
 use lsp_types::{
     notification::PublishDiagnostics, request::GotoDefinition, DiagnosticSeverity,
@@ -7,28 +8,111 @@ use lsp_types::{
 
 use lsp_server::{Connection, ExtractError, Message, Notification, Request, RequestId, Response};
 
-fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
-    // Note that  we must have our logging only write out to stderr.
-    eprintln!("Starting generic LSP server");
+// fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
+//     // Note that  we must have our logging only write out to stderr.
+//     eprintln!("Starting generic LSP server");
 
-    let (connection, io_threads) = Connection::stdio();
+//     let (connection, io_threads) = Connection::stdio();
 
-    let server_capabilities = serde_json::to_value(&ServerCapabilities {
-        // completion_provider: Some(lsp_types::CompletionOptions::default()),
-        diagnostic_provider: Some(lsp_types::DiagnosticServerCapabilities::Options(
-            lsp_types::DiagnosticOptions {
-                identifier: Some(String::from("spelgud")),
-                ..Default::default()
-            },
-        )),
-        ..Default::default()
-    })
-    .unwrap();
-    let initialization_params = connection.initialize(server_capabilities)?;
-    main_loop(connection, initialization_params)?;
-    io_threads.join()?;
+//     let server_capabilities = serde_json::to_value(&ServerCapabilities {
+//         // completion_provider: Some(lsp_types::CompletionOptions::default()),
+//         diagnostic_provider: Some(lsp_types::DiagnosticServerCapabilities::Options(
+//             lsp_types::DiagnosticOptions {
+//                 identifier: Some(String::from("spelgud")),
+//                 ..Default::default()
+//             },
+//         )),
+//         ..Default::default()
+//     })
+//     .unwrap();
+//     let initialization_params = connection.initialize(server_capabilities)?;
+//     main_loop(connection, initialization_params)?;
+//     io_threads.join()?;
 
-    eprintln!("Shutting down server");
+//     eprintln!("Shutting down server");
+//     Ok(())
+// }
+
+struct Correction {
+    start: u32,
+    end: u32,
+}
+
+fn send_line(
+    input: &str,
+    writer: &mut std::process::ChildStdin,
+    reader: &mut BufReader<std::process::ChildStdout>,
+) -> Result<String, Box<dyn Error>> {
+    eprint!("Sending to aspell: {}", input);
+    writer.write(input.as_bytes())?;
+    for line in reader.lines() {
+        let ln = line?;
+        eprintln!("Got {}", ln);
+        if ln.is_empty() {
+            return Ok(String::new());
+        }
+
+        let mut splits = ln.split_whitespace();
+        match splits.next() {
+            Some("&") => {
+                // & badword suggestion_count offset: suggestion1, suggestion2, ...
+                let word = splits.next().unwrap();
+
+                // skip suggestion count
+                if splits.next().is_none() {
+                    Err("Early end of line")?
+                }
+
+                let offset = splits
+                    .next()
+                    .unwrap()
+                    .to_string()
+                    .strip_suffix(":")
+                    .unwrap()
+                    .parse::<u32>()?;
+                eprintln!("Got mispell word:{} offset:{}", word, offset);
+            }
+            Some(sym) => {
+                Err(format!("Unexpected aspell prefix: {}", sym))?;
+            }
+            None => {
+                return Ok(String::new());
+            }
+        }
+    }
+    Ok(String::new())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut aspell = std::process::Command::new("aspell")
+        .arg("pipe")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    eprintln!("Started aspell with pid {}", aspell.id());
+    // let mut stdin = aspell.stdin.take().expect("Failed to open stdin");
+    // let mut stdout = aspell.stdout.take().expect("Failed to open stdin");
+    let mut stdin = aspell.stdin.take().unwrap();
+    let stdout = aspell.stdout.take().unwrap();
+    let mut reader = BufReader::new(stdout);
+
+    // put aspell into "terse" mode, so it does not send "*" for correct words
+    stdin.write_all("!\n".as_bytes())?;
+
+    // first line from aspell is version info
+    let mut intro = String::new();
+    reader.read_line(&mut intro)?;
+    eprintln!("aspell: {}", intro);
+
+    {
+        let res = send_line("henlo hai world world\n", &mut stdin, &mut reader)?;
+        eprintln!("read '{}'", res);
+    }
+    {
+        let res = send_line("wurld\n", &mut stdin, &mut reader)?;
+        eprintln!("read '{}'", res);
+    }
+
     Ok(())
 }
 
